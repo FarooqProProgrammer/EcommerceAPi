7c6dfeabea66c01cdf7348c366d259feb82b1a38
FarooqProProgrammer <107787520+FarooqProProgrammer@users.noreply.github.com>
2024-01-28 22:19:20 +0500

    Updated Code

--- config/db.js
+++ config/db.js
@@ -1,2 +1,4 @@
 const mongoose = require("mongoose");
-module.exports = mongoose.connect('mongodb+srv://farooq123:farooq123@cluster0.ijdh8yd.mongodb.net/Ecommerce?retryWrites=true&w=majority');;
+require('dotenv').config();
+
+module.exports = mongoose.connect(process.env.MONGO_URI);
